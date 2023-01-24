621c9095c03e329aa4bcc7133b1b0763978b2d1e
Leisan Latypova <latley90@gmail.com>
2023-01-24 14:48:47 +0300

    Initial Commit

---
+++ ChallengeLesson5/ChallengeLesson5App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeLesson5App.swift
+//  ChallengeLesson5
+//
+//  Created by Leysan Latypova on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeLesson5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
